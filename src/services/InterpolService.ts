5af367911a907a602d127f0f898f0423a2477ad1
Jakub Rekowski <jakub.rekowski@gmail.com>
2024-10-20 18:59:43 +0200

    feat: added getRedNoticeDetailImages

--- src/services/InterpolService.ts
+++ src/services/InterpolService.ts
@@ -1,6 +1,8 @@
 import type { CancelablePromise } from "../core/CancelablePromise";
 import { OpenAPI } from "../core/OpenAPI";
 import { request as __request } from "../core/request";
+import { RedNoticeDetailImagesEntitiy } from "../models/RedNoticeDetailImages";
+import { RedNoticeDetailsEntitiy } from "../models/RedNoticeDetails";
 import type { RedNoticesEntitiy, RedNoticesQuery } from "../models/RedNotices";
 
 export class InterpolService {
@@ -12,10 +14,22 @@ export class InterpolService {
     });
   }
 
-  public static getRedNoticeDetails(noticeID: string): CancelablePromise<RedNoticesEntitiy> {
+  public static getRedNoticeDetails(noticeID: string): CancelablePromise<RedNoticeDetailsEntitiy> {
     return __request(OpenAPI, {
       method: "GET",
       url: "/notices/v1/red/{noticeID}",
+      path: {
+        noticeID,
+      },
+    });
+  }
+
+  public static getRedNoticeDetailImages(
+    noticeID: string,
+  ): CancelablePromise<RedNoticeDetailImagesEntitiy> {
+    return __request(OpenAPI, {
+      method: "GET",
+      url: "/notices/v1/red/{noticeID}/images",
       path: {
         noticeID,
       },
